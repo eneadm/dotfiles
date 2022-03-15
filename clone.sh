ddff474c7c1610ec3f0f214c49bba62771588b09
Enea Dhiamandi <enea@dhiamandi.com>
2022-03-15 21:06:13 +0100

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -2,12 +2,12 @@
 
 echo "Cloning repositories..."
 
-WORK=$HOME/_code
+CODE=$HOME/_code
 
 # Company
-git clone git@bitbucket.org:ktronik/klimahelden-frontend.git $WORK/klimahelden-frontend
-git clone git@bitbucket.org:ktronik/klimahelden-backend.git $WORK/klimahelden-backend
-git clone git@bitbucket.org:ktronik/klimahelden-calculation-service.git $WORK/klimahelden-calculation-service
-git clone git@bitbucket.org:ktronik/klimahelden-latextopdf-service.git $WORK/klimahelden-latextopdf-service
+git clone git@bitbucket.org:ktronik/klimahelden-frontend.git $CODE/klimahelden-frontend
+git clone git@bitbucket.org:ktronik/klimahelden-backend.git $CODE/klimahelden-backend
+git clone git@bitbucket.org:ktronik/klimahelden-calculation-service.git $CODE/klimahelden-calculation-service
+git clone git@bitbucket.org:ktronik/klimahelden-latextopdf-service.git $CODE/klimahelden-latextopdf-service
 git clone git@bitbucket.org:ktronik/klimahelden-website.git $WORK/vrata
-git clone git@bitbucket.org:ktronik/vrata.git $WORK/vrata
+git clone git@bitbucket.org:ktronik/vrata.git $CODE/vrata
